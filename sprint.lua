02206a2fa38bce1da16c87c74b89463283debdbd
Pedrin <97545769+PedrinBlox@users.noreply.github.com>
2023-08-02 20:33:46 -0300

    Update sprint.lua

--- sprint.lua
+++ sprint.lua
@@ -7,6 +7,16 @@ local player = Players.LocalPlayer
 local speed_fast = 50
 local speed_normal = 16
 
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "Sprint Script";
+    Text = "Made by pedrin (github.com/PedrinBlox)";
+    Duration = math.huge;
+    Button1 = "Ok";
+    Callback = function(notification)
+        notification:Destroy()
+    end
+})
+
 UserInputService.InputBegan:Connect(function(input)
     if input.KeyCode == Enum.KeyCode.LeftShift or input.KeyCode == Enum.KeyCode.RightShift then
         if player.Character and player.Character:FindFirstChildOfClass("Humanoid") then
